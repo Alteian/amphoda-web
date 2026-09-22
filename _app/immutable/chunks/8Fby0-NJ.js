95037b47d16138f1302e4c824c346468c7a2abe2
David Roučka <Alteian@proton.me>
2026-09-22 13:01:28 +0200

    Preserve hash on language toggle.

---
+++ _app/immutable/chunks/8Fby0-NJ.js
@@ -0,0 +1,1 @@
+import{b as o}from"./BktIBZ4-.js";import{r}from"./X3XCzaVv.js";const t="";function a(...e){if(!e[0].startsWith("/"))throw new Error(`Cannot use \`resolve(...)\` with a non-absolute pathname or route ID (got "${e[0]}"). \`resolve\` is only for internal pathnames and route IDs; external URLs should be used directly.`);return o+t+r(e[0],e[1])}export{a as r};
