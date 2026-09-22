6e8f658c880957068f72ed3951e583f15084fe69
David Roučka <Alteian@proton.me>
2026-09-22 15:19:51 +0200

    Deploy SEO/AEO/GEO pack: llms, unique OGs, robots, manifests.

--- _app/immutable/chunks/BX_Oaqu5.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./BKPhW_4B.js";import{r}from"./B4HkN7NP.js";const t="";function a(...e){if(!e[0].startsWith("/"))throw new Error(`Cannot use \`resolve(...)\` with a non-absolute pathname or route ID (got "${e[0]}"). \`resolve\` is only for internal pathnames and route IDs; external URLs should be used directly.`);return o+t+r(e[0],e[1])}export{a as r};
